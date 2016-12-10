86faaec7c5d04cc9abb1a9fe763a9c82bbde6551
pshakya2 <pratistha_shakya@brown.edu>
2016-12-10 18:12:37 -0500

    Update widget.h

--- widget.h
+++ widget.h
@@ -4,9 +4,24 @@
 
 #include <QtWidgets/QWidget>
 #include <QtCharts/QChartGlobal>
+#include <QtCharts/QValueAxis>
 #include <QFile>
+#include <QDebug>
+#include <QAudioOutput>
 #include <QTimer>
 #include <QPushButton>
+#include <QTextEdit>
+#include <QToolButton>
+#include <QTextStream>
+#include <QAudioRecorder>
+#include <qcustomplot/qcustomplot.h>
+#include <QMediaPlayer>
+#include <QElapsedTimer>
+#include <QAudioBuffer>
+#include <fstream>
+#include <QVector>
+#include <string>
+#include <ctime>
 
 QT_CHARTS_BEGIN_NAMESPACE
 class QLineSeries;
@@ -28,18 +43,44 @@ class Widget : public QWidget
 public:
     Widget(QWidget *parent = 0);
     ~Widget();
+    void initialize_GUI();
+    void Play();
+    void Read_File();
+    void Write_scale();
+    void Write_File();
+    void updateText();
+    int samplecount=0;
+    int endofFile=0; //1 is end of file
+    QVector<double> data;
+    QVector<double> samples;
 
 private slots:
-    void handleButton();
+    void handleStart();
     void handleStop();
+    void FreqSelect();
 
 private:
-    IOdevice *mic;
+    IOdevice *mic, *speaker;
     QChart *input_plot;
+    QChart *output_plot;
     QLineSeries *dataseries;
+    QByteArray *audio_data;
+    QLineSeries *dataseries2;
     QAudioInput *audioInput;
+    QAudioOutput *audioOutput;
     QFile outputfile;
-    QPushButton *start_button, *stop_button;
+    QPushButton *start_button, *stop_button, *Freq_select;
+    QTextEdit *Text1,*Text2;
+    QWidget *widget;
+    QToolButton *b;
+    QAudioBuffer *buffer;
+    QTextStream *stream;
+    QAudioRecorder *audioRecord;
+    QMediaPlayer *player;
+    clock_t start,finish;
+    QCustomPlot *customPlot;
+
 };
 
 #endif // WIDGET_H
+
